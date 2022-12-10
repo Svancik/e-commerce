c8f7ee83fba230f60e9448c4c3485b184f5c65e3
Svancik <thesvancik@gmail.com>
2022-12-10 23:23:42 +0100

    featured product & card

---
+++ src/components/featuredProducts/FeaturedProducts.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Card } from "../card/Card";
+import "./featuredProducts.scss";
+
+export const FeaturedProducts = ({ type }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long Sleeve Graphic T-shirt",
+      isNew: true,
+      isNaN: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long Sleeve Graphic T-shirt",
+      isNaN: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Skirt",
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Hat",
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta,
+          ipsam nisi voluptatibus distinctio suscipit porro possimus sit libero,
+          esse nobis velit asperiores magni. Porro nemo modi repellat temporibus
+          nisi amet odit delectus et expedita perferendis. Quasi nihil, dolor,
+          cumque cupiditate labore quae ullam, voluptatum corrupti velit harum
+          iste sequi vitae.
+        </p>
+      </div>
+      <div className="bottom">
+        {data.map((item) => (
+          <Card item={item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { FeaturedProducts } from "../../components/featuredProducts/FeaturedProducts";
 import Slider from "../../components/slider/Slider";
 import "./home.scss";
 export const Home = () => {
   return (
     <div className="home">
       <Slider />
+      <FeaturedProducts type="featured" />
+      <FeaturedProducts type="trending" />
     </div>
   );
 };
