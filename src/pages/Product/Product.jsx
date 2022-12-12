d0f28b5c6e7423c2ae25a37f9a1cd54ac412d1ea
Svancik <thesvancik@gmail.com>
2022-12-12 19:36:30 +0100

    single product page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,6 +1,80 @@
 import React from "react";
+import { useState } from "react";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 import "./product.scss";
+import { FavoriteBorderOutlinedIcon } from "@mui/icons-material/FavoriteBorderOutlined";
 
 export const Product = () => {
-  return <div>Product</div>;
+  const [slectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+
+  const images = [
+    "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazdy=load",
+    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazdy=load",
+  ];
+
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[slectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className="price">$199</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+          doloremque numquam, vel debitis atque a itaque soluta obcaecati hic
+          voluptate exercitationem ea excepturi, iusto tempora deserunt omnis
+          fuga. Eos dolor magnam illum tenetur minus? Dolorem quae officiis
+          consequuntur laborum qui tempore repellendus, voluptates consequatur
+          omnis?
+        </p>
+        <div className="quantity">
+          <button
+            onClick={
+              quantity === 0
+                ? () => setQuantity(0)
+                : () => setQuantity((prev) => prev - 1)
+            }
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon /> ADD TO CARD
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISH LIST
+          </div>
+          <div className="item">
+            <BalanceIcon /> ADD TO COMPARE
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
+    </div>
+  );
 };
