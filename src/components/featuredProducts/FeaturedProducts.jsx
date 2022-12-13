968ee73fa25e199a7d88ab99e3c62df270d8f339
Svancik <thesvancik@gmail.com>
2022-12-13 21:44:27 +0100

    fetching data

--- src/components/featuredProducts/FeaturedProducts.jsx
+++ src/components/featuredProducts/FeaturedProducts.jsx
@@ -1,33 +1,12 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import { Card } from "../card/Card";
 import "./featuredProducts.scss";
+import useFetch from "./../../hooks/useFetch";
 
 export const FeaturedProducts = ({ type }) => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        //Tento header s tokenem musíme poslat v rámci autorizace, jinak nám nedovolí poslat data (token jsme získali te Strapi)
-        const res = await axios.get(
-          process.env.REACT_APP_API_URL + "/products?populate=*",
-          {
-            headers: {
-              Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
-            },
-          }
-        );
-        setData(res.data.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, []);
-
-  console.log(data);
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
 
   return (
     <div className="featuredProducts">
@@ -43,9 +22,11 @@ export const FeaturedProducts = ({ type }) => {
         </p>
       </div>
       <div className="bottom">
-        {data.map((item) => (
-          <Card item={item} key={item.id} />
-        ))}
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
       </div>
     </div>
   );
