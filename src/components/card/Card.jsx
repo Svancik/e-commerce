d4385d666ce88806be129c647b99c3487cf1e6f8
Svancik <thesvancik@gmail.com>
2022-12-14 19:02:27 +0100

    filtering and sorting fetched data

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,11 +7,11 @@ export const Card = ({ item }) => {
     <Link className="link" to={`/product/${item.id}`}>
       <div className="card">
         <div className="image">
-          {item?.attributes.isNew && <span>New Season</span>}
+          {item?.attributes?.isNew && <span>New Season</span>}
           <img
             src={
               process.env.REACT_APP_UPLOAD_URL +
-              item.attributes.img.data.attributes.url
+              item?.attributes?.img?.data?.attributes?.url
             }
             alt=""
             className="mainImg"
@@ -19,7 +19,7 @@ export const Card = ({ item }) => {
           <img
             src={
               process.env.REACT_APP_UPLOAD_URL +
-              (item.attributes.img2.data === null
+              (item?.attributes?.img2?.data === null
                 ? item.attributes?.img?.data?.attributes?.url
                 : item.attributes?.img2?.data?.attributes?.url)
             }
@@ -28,10 +28,10 @@ export const Card = ({ item }) => {
           />
         </div>
       </div>
-      <h2>{item?.attributes.title}</h2>
+      <h2>{item?.attributes?.title}</h2>
       <div className="prices">
-        <h3>${item.oldPrice || item?.attributes.price + 20}</h3>
-        <h3>${item?.attributes.price}</h3>
+        <h3>${item?.oldPrice || item?.attributes?.price + 20}</h3>
+        <h3>${item?.attributes?.price}</h3>
       </div>
     </Link>
   );
